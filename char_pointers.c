fb9ba6e123fe28d11cda27ee781f178cd528d145
Shaurya Shrivastava <54743877+z3rofactorial@users.noreply.github.com>
2022-09-10 08:24:16 +0530

    Add files via upload

---
+++ char_pointers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char C[5];
+    C[0]='J';
+    C[1]='O';    
+    C[2]='H';
+    C[3]='N';
+    C[4]='a';
+
+    int slen = strlen(C);
+    printf("%s \nLength=%d",C,len);
+}
